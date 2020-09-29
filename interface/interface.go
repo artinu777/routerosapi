9577f7b249f89f91357e1a75872a0360d2cfca2f
Пётр Янус <pyanus1@mts.ru>
2020-09-29 17:53:48 +0300

    d

---
+++ interface/interface.go
@@ -0,0 +1,68 @@
+package inter
+
+
+import (
+"flag"
+"fmt"
+"log"
+"strings"
+
+"github.com/go-routeros/routeros"
+)
+
+type Inter struct {
+	client *routeros.Client
+}
+
+func (receiver *Inter) Print() {
+	properties := flag.String("properties", "name,rx-byte,tx-byte,rx-packet,tx-packet", "Properties")
+	reply, err := receiver.client.Run("/interface/print", "?disabled=false", "?running=true", "=.proplist="+*properties)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, re := range reply.Re {
+		for _, p := range strings.Split(*properties, ",") {
+			fmt.Print(re.Map[p], "\t")
+		}
+		fmt.Print("\n")
+	}
+	fmt.Print("\n")
+}
+
+func (receiver *Inter) Set() {
+
+}
+
+func (receiver *Inter) Remove() {
+}
+
+func (receiver *Inter) Add() {
+
+}
+
+func (receiver *Inter) Find() {
+
+}
+
+func (receiver *Inter) Get() {
+
+}
+
+func (receiver *Inter) Export() {
+
+}
+
+func (receiver *Inter) Enable() {
+
+}
+
+func (receiver *Inter) Disable() {
+
+}
+
+func (receiver *Inter) Comment() {}
+
+func (receiver *Inter) Move() {
+
+}
---
+++ router.go
@@ -0,0 +1,61 @@
+package routeosapi
+
+import (
+	"errors"
+
+	"github.com/go-routeros/routeros"
+
+	inter "github.com/artinu777/routerosapi/interface"
+	"github.com/artinu777/routerosapi/ip"
+)
+
+type Router struct {
+	routeAuth  *RouterAuth
+	client     *routeros.Client
+	Ip         ip.Ip
+	_Interface inter.Inter
+}
+
+type RouterAuth struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+func NewRouter() *Router {
+	return &Router{}
+}
+
+func (receiver *Router) SetAuth(auth RouterAuth) (*Router, error) {
+	if auth.User == "" && auth.Password == "" {
+		return receiver, errors.New("auth empty")
+	}
+
+	receiver.routeAuth = &auth
+	return receiver, nil
+}
+
+func (receiver *Router) Connect(address string) (*Router, error) {
+	if receiver.routeAuth == nil {
+		return nil, errors.New("nil auth")
+	}
+
+	if address == "" {
+		return nil, errors.New("address empty")
+	}
+
+	c, err := routeros.Dial(address, receiver.routeAuth.User, receiver.routeAuth.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	receiver.client = c
+	return receiver, nil
+}
+
+func (receiver *Router) name() {
+
+}
+
+func Example() {
+
+}
